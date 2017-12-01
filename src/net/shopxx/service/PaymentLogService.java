eb1a0de08137ae8f7e0315f1e8ed21235c05f87c
leejackou <jackou@qq.com>
2017-12-01 10:12:50 +0800

    提交代码

---
+++ src/net/shopxx/service/PaymentLogService.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2005-2015 shopxx.net. All rights reserved.
+ * Support: http://www.shopxx.net
+ * License: http://www.shopxx.net/license
+ */
+package net.shopxx.service;
+
+import net.shopxx.entity.PaymentLog;
+public interface PaymentLogService extends BaseService<PaymentLog, Long> {
+	PaymentLog findBySn(String sn);
+	void handle(PaymentLog paymentLog);
+
+}
